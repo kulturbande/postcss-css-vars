fad7ffe044d3768c4ce852047d682bf20c5fc166
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2019-12-30 16:53:41 +0100

    change test structure

---
+++ src/tests/helpers.ts
@@ -0,0 +1,28 @@
+import { readFile } from 'fs';
+import { join } from 'path';
+import postcss from 'postcss';
+import plugin from '..';
+
+async function readFixture(name: string, postfix: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        readFile(
+            join(__dirname, 'fixtures', name + '.' + postfix + '.css'),
+            { encoding: 'UTF-8' },
+            (err, data: string) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(data);
+            }
+        );
+    });
+}
+
+export async function testFixture(fixtureName: string, opts: object = {}) {
+    const acceptedPlugin: postcss.AcceptedPlugin = plugin(opts);
+    let result = await postcss([acceptedPlugin]).process(await readFixture(fixtureName, 'input'), {
+        from: undefined,
+    });
+    expect(result.css).toEqual(await readFixture(fixtureName, 'output'));
+    expect(result.warnings()).toHaveLength(0);
+}
--- src/tests/integration.test.ts
+++ src/tests/integration.test.ts
@@ -1,15 +1,7 @@
-import postcss from 'postcss';
-import plugin from '..';
+import { testFixture } from './helpers';
 
-async function run(input: string, output: string, opts: object) {
-    const acceptedPlugin: postcss.AcceptedPlugin = plugin(opts);
-    let result = await postcss([acceptedPlugin]).process(input, {
-        from: undefined,
+describe('Direct usage', () => {
+    it('should remove the variable', async () => {
+        await testFixture('direct_usage/simple');
     });
-    expect(result.css).toEqual(output);
-    expect(result.warnings()).toHaveLength(0);
-}
-
-it('does something', async () => {
-    await run('a{ --color: red; color: var(--color); }', 'a{ color: red; }', {});
 });
