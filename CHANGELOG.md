b3f0422cd710f4d2f410a4fb785dfecf2c9eb6f8
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2020-01-04 11:38:49 +0100

    added more test (one break atm.)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added
+
+-   more media query tests
+
+### Changed
+
+-   fixed typos in several classes
+
 ## [0.0.3] - 2020-01-03
 
 ### Added
--- src/tests/integration.test.ts
+++ src/tests/integration.test.ts
@@ -15,11 +15,11 @@ describe('Direct usage', () => {
 });
 
 describe('Root Level', () => {
-    it('should basicly work for :root - pseudo selector', async () => {
+    it('should basically work for :root - pseudo selector', async () => {
         await testFixture('root_level/root');
     });
 
-    it('should basicly work for body selector', async () => {
+    it('should basically work for body selector', async () => {
         await testFixture('root_level/body');
     });
 });
@@ -29,7 +29,7 @@ describe('Permutation', () => {
         await testFixture('permutation/class');
     });
 
-    it("should use global variables if the varaible isn't avaiable", async () => {
+    it("should use global variables if the variable isn't available", async () => {
         await testFixture('permutation/multiple_variables');
     });
 });
@@ -37,5 +37,17 @@ describe('Permutation', () => {
 describe('Media Queries', () => {
     it('should support media queries', async () => {
         await testFixture('media_queries/simple');
+    });
+
+    it('should support simple multiple media queries', async () => {
+        await testFixture('media_queries/multiple_media_queries');
+    });
+
+    it('should support media query with multiple variables', async () => {
+        await testFixture('media_queries/media_query_multiple_variables');
+    });
+
+    it('should support multiple (complex) media queries', async () => {
+        await testFixture('media_queries/multiple_complex');
     });
 });
